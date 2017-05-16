a49a3c7d2ea74bb9d5182f41e6f7bf9994d73807
huhx0015 <mhuh@yoonhuh.com>
2017-05-16 10:20:01 -0700

    AlchenomiconImageUtils: Added more resource references. Updated README.md.

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
 DragonAlchenomicon
 ==================
 
-PROGRAMMER: huhx0015
+DEVELOPER: huhx0015
 
-Dragon Alchenomicon is an Android application in which a user can look up alchemy recipes from the Dragon Quest VIII and Dragon Quest IX RPG games.
+### DRAGON ALCHENOMICON
+![dragonalchenomicon_preview](https://cloud.githubusercontent.com/assets/1645482/26118558/3ec1b5c6-3a1f-11e7-81b1-d899c63cecc5.gif)
 
+## Description
 
-Sprites Ripped By: quasi_stomach
+Dragon Alchenomicon: An Android application used for looking up alchemy recipes for Square Enix's RPG game, Dragon Quest VIII. Dragon Alchenomicon was developed utilizing MVP architecture, in conjunction with Dagger 2.
+
+## Resources
+
+### Sprites
+
+Icons Ripped By: quasi_stomach
+
+### Libraries
+
+* Android SQLiteAssetHelper: https://github.com/jgilfelt/android-sqlite-asset-helper
+* Butter Knife: http://jakewharton.github.io/butterknife/
+* Dagger 2: https://github.com/google/dagger
+* Glide: https://github.com/bumptech/glide
+* RxJava 2: https://github.com/ReactiveX/RxJava
+* RxAndroid: https://github.com/ReactiveX/RxAndroid
+
+## Copyright
+
+DRAGON ALCHENOMICON is a fan application and is not associated with or endorsed by SQUARE ENIX.
+
+© ARMOR PROJECT/BIRD STUDIO/SQUARE ENIX All Rights Reserved.
+
+© DRAGON QUEST, DRAGON WARRIOR are registered trademarks of Square Enix Co., Ltd.
+
+All DRAGON WARRIOR, DRAGON QUEST images, logos, names, music, and related media are intellectual properties of SQUARE ENIX unless otherwise noted.
--- app/src/main/java/com/huhx0015/dragonalchenomicon/utils/AlchenomiconImageUtils.java
+++ app/src/main/java/com/huhx0015/dragonalchenomicon/utils/AlchenomiconImageUtils.java
@@ -1,8 +1,5 @@
 package com.huhx0015.dragonalchenomicon.utils;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.util.DisplayMetrics;
 import com.huhx0015.dragonalchenomicon.R;
 
 /**
@@ -17,52 +14,245 @@ public class AlchenomiconImageUtils {
 
         switch (itemName) {
 
-            case "Medicinal Herb":
-                return R.drawable.dq8_item_257;
+            case "Agility Ring":
+                return R.drawable.dq8_item_251;
 
             case "Angel Cheese":
                 return R.drawable.dq8_item_327;
 
-            case "Plain Cheese":
-                return R.drawable.dq8_item_310;
+            case "Amor Seco Essence":
+                return R.drawable.dq8_item_272;
+
+            case "Angel Robe":
+                return R.drawable.dq8_item_155;
+
+            case "Antidotal Herb":
+                return R.drawable.dq8_item_258;
+
+            case "Bandana":
+                return R.drawable.dq8_item_186;
+
+            case "Bandit Axe":
+                return R.drawable.dq8_item_55;
+
+            case "Blizzard Blade":
+                return R.drawable.dq8_item_33;
+
+            case "Boxer Shorts":
+                return R.drawable.dq8_item_106;
+
+            case "Bunny Ears":
+                return R.drawable.dq8_item_193;
+
+            case "Bunny Suit":
+                return R.drawable.dq8_item_113;
 
             case "C-C-Cold Cheese":
                 return R.drawable.dq8_item_319;
 
+            case "Catholicon Ring":
+                return R.drawable.dq8_item_254;
+
             case "Chilly Cheese":
                 return R.drawable.dq8_item_317;
+
+            case "Chimera Wing":
+                return R.drawable.dq8_item_273;
+
+            case "Chunky Cheese":
+                return R.drawable.dq8_item_330;
 
             case "Cold Cheese":
                 return R.drawable.dq8_item_318;
 
+            case "Cowpat":
+                return R.drawable.dq8_item_298;
+
+            case "Cool Cheese":
+                return R.drawable.dq8_item_316;
+
+            case "Cured Cheese":
+                return R.drawable.dq8_item_326;
+
+            case "Divine Bustier":
+                return R.drawable.dq8_item_115;
+
             case "Dragon Dung":
                 return R.drawable.dq8_item_299;
 
-            case "Special Medicine":
-                return R.drawable.dq8_item_261;
-
-            case "Lesser Panacea":
-                return R.drawable.dq8_item_262;
-
-            case "Greater Panacea":
-                return R.drawable.dq8_item_263;
+            case "Elfin Elixir":
+                return R.drawable.dq8_item_276;
 
             case "Fresh Milk":
                 return R.drawable.dq8_item_305;
 
+            case "Fur Hood":
+                return R.drawable.dq8_item_196;
+
+            case "Gold Nugget":
+                return R.drawable.dq8_item_296;
+
+            case "Greater Panacea":
+                return R.drawable.dq8_item_263;
+
+            case "Gold Rosary":
+                return R.drawable.dq8_item_244;
+
+            case "Hard Cheese":
+                return R.drawable.dq8_item_328;
+
+            case "Iron Nail":
+                return R.drawable.dq8_item_297;
+
+            case "Lesser Panacea":
+                return R.drawable.dq8_item_262;
+
+            case "Life Bracer":
+                return R.drawable.dq8_item_230;
+
+            case "Magic Beast Hide":
+                return R.drawable.dq8_item_304;
+
+            case "Magic Water":
+                return R.drawable.dq8_item_274;
+
+            case "Medicinal Herb":
+                return R.drawable.dq8_item_257;
+
+            case "Metal King Armor":
+                return R.drawable.dq8_item_148;
+
+            case "Mild Cheese":
+                return R.drawable.dq8_item_325;
+
+            case "Moon's Mercy":
+                return R.drawable.dq8_item_268;
+
+            case "Moonwort Bulb":
+                return R.drawable.dq8_item_259;
+
+            case "Mystifying Mixture":
+                return R.drawable.dq8_item_269;
+
+            case "Nook Grass":
+                return R.drawable.dq8_item_352;
+
+            case "Orichalcum":
+                return R.drawable.dq8_item_291;
+
+            case "Phantom Mask":
+                return R.drawable.dq8_item_216;
+
+            case "Plain Cheese":
+                return R.drawable.dq8_item_310;
+
+            case "Premium Mould":
+                return R.drawable.dq8_item_309;
+
+            case "Red Mould":
+                return R.drawable.dq8_item_307;
+
+            case "Rennet Powder":
+                return R.drawable.dq8_item_306;
+
+            case "Rock Salt":
+                return R.drawable.dq8_item_300;
+
+            case "Rockbomb Shard":
+                return R.drawable.dq8_item_277;
+
+            case "Rose Root":
+                return R.drawable.dq8_item_264;
+
+            case "Rose-Wort":
+                return R.drawable.dq8_item_265;
+
+            case "Sage's Stone":
+                return R.drawable.dq8_item_278;
+
+            case "Saint's Ashes":
+                return R.drawable.dq8_item_302;
+
+            case "Scholar's Specs":
+                return R.drawable.dq8_item_237;
+
+            case "Scorching Cheese":
+                return R.drawable.dq8_item_314;
+
+            case "Slime Earrings":
+                return R.drawable.dq8_item_239;
+
+            case "Soft Cheese":
+                return R.drawable.dq8_item_329;
+
+            case "Seed of Agility":
+                return R.drawable.dq8_item_281;
+
+            case "Seed of Defense":
+                return R.drawable.dq8_item_282;
+
+            case "Seed of Life":
+                return R.drawable.dq8_item_284;
+
+            case "Seed of Magic":
+                return R.drawable.dq8_item_285;
+
+            case "Seed of Strength":
+                return R.drawable.dq8_item_280;
+
+            case "Seed of Wisdom":
+                return R.drawable.dq8_item_283;
+
+            case "Special Antidote":
+                return R.drawable.dq8_item_267;
+
+            case "Special Medicine":
+                return R.drawable.dq8_item_261;
+
+            case "Shimmering Dress":
+                return R.drawable.dq8_item_119;
+
+            case "Spicy Cheese":
+                return R.drawable.dq8_item_312;
+
+            case "Stone Hardhat":
+                return R.drawable.dq8_item_194;
+
+            case "Super Spicy Cheese":
+                return R.drawable.dq8_item_313;
+
+            case "Strength Ring":
+                return R.drawable.dq8_item_224;
+
+            case "Strong Antidote":
+                return R.drawable.dq8_item_266;
+
+            case "Strong Medicine":
+                return R.drawable.dq8_item_260;
+
+            case "Thief's Key":
+                return R.drawable.dq8_item_288;
+
+            case "Timbrel of Tension":
+                return R.drawable.dq8_item_293;
+
+            case "Turban":
+                return R.drawable.dq8_item_191;
+
+            case "Waterweed Mould":
+                return R.drawable.dq8_item_308;
+
+            case "Yggdrasil Dew":
+                return R.drawable.dq8_item_271;
+
+            case "Yggdrasil Leaf":
+                return R.drawable.dq8_item_270;
+
+            case "Wing of Bat":
+                return R.drawable.dq8_item_303;
+
             default:
                 return R.drawable.dq8_item_332;
         }
-    }
-
-    /** CONVERSION METHODS _____________________________________________________________________ **/
-
-    // convertPixelsToDp: This method converts device specific pixels to density independent pixels.
-    // Source: http://stackoverflow.com/questions/4605527/converting-pixels-to-dp
-    public static float convertPixelsToDp(float px, Context context){
-        Resources resources = context.getResources();
-        DisplayMetrics metrics = resources.getDisplayMetrics();
-        float dp = px / ((float) metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
-        return dp;
     }
 }
